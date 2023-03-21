20b27db3cbf1b7c0a0c8b204c12cf7995ab61ca9
Büşra Keskin <wwbusrakeskin@gmail.com>
2023-03-21 22:56:37 +0300

    Added timeline, animation and imported some assets

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        Debug.Log("sag sol yapiyorummm");
+        float verticalAxis = Input.GetAxis("Vertical");
+        Debug.Log("s ve w kullanıyorummm");
+
+
+    }
+}
