0d4c00d9b5b6f5fbedd90658248392100f4a8f22
Büşra Keskin <wwbusrakeskin@gmail.com>
2023-03-26 16:33:39 +0300

    Moving the player with using New Input System

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] InputAction movement;
+    [SerializeField] float offset = 0.1f;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   void OnEnable()
+    {
+        movement.Enable();
+    }
+   void OnDisable()
+    {
+        movement.Disable();
+    }
     void Update()
     {
-        float horizontalAxis = Input.GetAxis("Horizontal");
-        Debug.Log("sag sol yapiyorummm");
-        float verticalAxis = Input.GetAxis("Vertical");
-        Debug.Log("s ve w kullanıyorummm");
+        float horizontalAxis = movement.ReadValue<Vector2>().x;
+        float verticalAxis = movement.ReadValue<Vector2>().y;
+
+        ////float horizontalAxis = Input.GetAxis("Horizontal");
+        //Debug.Log(horizontalAxis);
+        ////float verticalAxis = Input.GetAxis("Vertical");
+        //Debug.Log(verticalAxis);
+
+        transform.localPosition = new Vector3(
+            transform.localPosition.x + offset,
+            transform.localPosition.y,
+            transform.localPosition.z);
 
 
     }
